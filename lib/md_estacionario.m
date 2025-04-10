2b7e9fbb7b413aa1e5a34eca519ed6e3b1985788
Wilson González Vanegas <wilgonzalez@utp.edu.co>
2025-04-10 00:03:49 -0500

    Actualizar md_estacionario
    Se actualiza el análisis para el modelo de datos en el estacionario en línea con la reestructuración del modelo de red.

--- lib/md_estacionario.m
+++ lib/md_estacionario.m
@@ -1,40 +1,40 @@
 classdef md_estacionario < hcs.modelo_datos
-% md_estacionario - Subclase para administrar el modelo de datos
-% en el estado estacionario.
-%
-% Esta subclase se encarga de separar y clasificar la información de un
-% caso segun su fuente de excitacion en estructuras de datos,
-% luego asigna handles a cada elemento de circuito presente en cada isla.
-%
-% Propiedades de md_estacionario:
-%	* circuito_cero_menos - estructura donde se guarda la informacion
-%	depurada de un circuito antes de la conmutacion de sus interruptores
-%
-% Metodos de md_estacionario:
-%	* extraer_info_estacionario(obj,caso) - evalua y conmuta los
-%	interruptores, luego propaga la nueva informacion, posteriormente se
-%	dispara la rutina que elimina elementos desenergizados en el estado
-%	estacionario finalmente separa la información del circuito por islas
-%	las cuales son estructuras que contiene un subconjunto de la
-%	información del caso original
-%
-%	* inicializar_estacionario(obj) - asigna los handles para cada puerta
-%	a cada de isla existente
-%
-% See also: md_transitorio, md_conmutacion, md_completo, 
-% md_estacionario_transitorio, md_estacionario_conmutacion
-    
-%   HACEOS - Herramienta para el Analisis de Circuitos Electricos de Orden Superior
-%   Copyright (c) 2025, Grupo de Investigacion en Campos Electromagneticos y Fenomenos Energeticos - CAFE
-%				        Universidad Tecnologica de Pereira (UTP)
-%  	Por: Brandon Ospina y Brian Gordon, CAFE-UTP
-%   y Wilson Gonzalez Vanegas, CAFE-UTP y Universidad Nacional de Colombia Sede Manizales
-%
-%   Este archivo es parte del proyecto HACEOS.
-%   Cubierto por la licencia 3-clause BSD (mas detalles en el archivo LICENSE).
-%   Vea https://github.com/HACEOS/ para mayor informacion.
-% 	
-%   Nota: este archivo es libre de tildes para evitar posibles conflictos en sistemas sin codificacion UTF-8
+    % md_estacionario - Subclase para administrar el modelo de datos
+    % en el estado estacionario.
+    %
+    % Esta subclase se encarga de separar y clasificar la información de un
+    % caso segun su fuente de excitacion en estructuras de datos,
+    % luego asigna handles a cada elemento de circuito presente en cada isla.
+    %
+    % Propiedades de md_estacionario:
+    %	* circuito_cero_menos - estructura donde se guarda la informacion
+    %	depurada de un circuito antes de la conmutacion de sus interruptores
+    %
+    % Metodos de md_estacionario:
+    %	* extraer_info_estacionario(obj,caso) - evalua y conmuta los
+    %	interruptores, luego propaga la nueva informacion, posteriormente se
+    %	dispara la rutina que elimina elementos desenergizados en el estado
+    %	estacionario finalmente separa la información del circuito por islas
+    %	las cuales son estructuras que contiene un subconjunto de la
+    %	información del caso original
+    %
+    %	* inicializar_estacionario(obj) - asigna los handles para cada puerta
+    %	a cada de isla existente
+    %
+    % See also: md_transitorio, md_conmutacion, md_completo,
+    % md_estacionario_transitorio, md_estacionario_conmutacion
+
+    %   HACEOS - Herramienta para el Analisis de Circuitos Electricos de Orden Superior
+    %   Copyright (c) 2025, Grupo de Investigacion en Campos Electromagneticos y Fenomenos Energeticos - CAFE
+    %				        Universidad Tecnologica de Pereira (UTP)
+    %  	Por: Brandon Ospina y Brian Gordon, CAFE-UTP
+    %   y Wilson Gonzalez Vanegas, CAFE-UTP y Universidad Nacional de Colombia Sede Manizales
+    %
+    %   Este archivo es parte del proyecto HACEOS.
+    %   Cubierto por la licencia 3-clause BSD (mas detalles en el archivo LICENSE).
+    %   Vea https://github.com/HACEOS/ para mayor informacion.
+    %
+    %   Nota: este archivo es libre de tildes para evitar posibles conflictos en sistemas sin codificacion UTF-8
 
     properties
         circuito_cero_menos
@@ -45,14 +45,14 @@ classdef md_estacionario < hcs.modelo_datos
             obj@hcs.modelo_datos();
         end
 
-        function obj = extraer_info_estacionario(obj, caso, mr)
+        function [obj, mr] = extraer_info_estacionario(obj, caso, mr)
             % Metodo para guardar la informacion original del caso antes de
             % ser depurada por otras subrutinas
             %   Entradas:
             %		caso (struct): estructura de datos donde se almacena
             %		    toda la informacion correspondiente a un circuito
             %		    electrico
-            %       mr (hcs.modelo_red) : 
+            %       mr (hcs.modelo_red) :
             %
             %	Salidas:
             %		> Ejecuta la evaluacion de interruptores
@@ -65,7 +65,7 @@ classdef md_estacionario < hcs.modelo_datos
             %       > Llena el campo de obj.id_elementos.estacionario.marcas_pol
             %       con los id's de las inductancias energizadas en el
             %       estado estacionario
-            
+
             [~,~,N_INT,ESTADO] = id_col_interruptores;
             obj.interruptores = obj.crear_interruptores(caso);
             obj.circuito_cero_menos = caso;
@@ -82,14 +82,27 @@ classdef md_estacionario < hcs.modelo_datos
                 end
             end
 
-            % Llamar el metodo detectar_elementos_energizados_estacionario
-            [id_elementos, id_marcas] = mr.detectar_elementos_energizados_estacionario(obj.circuito_cero_menos);
-            
-            % Llamar metodo detectar_islas
-            obj = mr.detectar_islas(obj.circuito_cero_menos, obj);
-            
-            obj.id_elementos.estacionario.puertas = id_elementos;  % Id de las puertas segun la columna "id_puerta" del caso
-            obj.id_elementos.estacionario.marcas_pol = id_marcas;  % Posiciones de fila en el campo .marcas_pol del caso
+            % Detectar elementos energizados y desenergizados
+            [gno_original, gno_depurado] = mr.detectar_elementos_energizados(obj.circuito_cero_menos);
+            %[id_elementos, id_marcas] = mr.detectar_elementos_energizados_estacionario(obj.circuito_cero_menos);
+
+            % Detectar islas en el circuito depurado
+            mr.detectar_islas(gno_depurado);
+
+            % Construir islas (subcasos) en el modelo de datos
+            islas_estacionario = mr.grafico_a_islas(obj.circuito_cero_menos, gno_depurado);
+
+            % Crear y almacenar versiones orientadas de los gráficos
+            go_original = mr.crear_grafico_orientado(gno_original);
+            go_depurado = mr.crear_grafico_orientado(gno_depurado);
+
+            %obj.id_elementos.estacionario.puertas = id_elementos;  % Id de las puertas segun la columna "id_puerta" del caso
+            %obj.id_elementos.estacionario.marcas_pol = id_marcas;  % Posiciones de fila en el campo .marcas_pol del caso
+            mr.graficos_no_orientados.originales.estacionario = gno_original;
+            mr.graficos_no_orientados.depurados.estacionario = gno_depurado;
+            mr.graficos_orientados.originales.estacionario = go_original;
+            mr.graficos_orientados.depurados.estacionario = go_depurado;
+            obj.islas.estacionario = islas_estacionario;
         end
 
         function obj = inicializar_estacionario(obj)
@@ -99,21 +112,23 @@ classdef md_estacionario < hcs.modelo_datos
             %		obj (struct): objeto que almacena toda la informacion
             %		referente al modelo de datos.
             %   Salidas:
-            %       > Recorre cada isla del circuito en el estado 
-            %       estacionario y etiqueta cada elemento con su 
-            %       respectiva clase (handle) 
-            
+            %       > Recorre cada isla del circuito en el estado
+            %       estacionario y etiqueta cada elemento con su
+            %       respectiva clase (handle)
+
             [R,L,C,LM,VI,II,VD,ID] = id_col_caso;                  % Definir indicadores para el ingreso de informacion
             [~, ~, ~, NAT_P] = id_col_puertas;                     % Definir apuntadores a las columnas del campo puertas
-            isla = {'DC','SIN','EXP','No_Fuente'};
+            isla = {'DC','SIN','EXP','NoFuente' 'Combinada'};
             for Y = 1:numel(isla)
-                if ~isempty(obj.islas.estacionario.(isla{Y}))
+                if ~isempty(fieldnames(obj.islas.estacionario.(isla{Y})))
+                    obj.islas.estacionario.(isla{Y}) = struct('puertas',[],...
+                                                              'marcas_pol',[]); 
                     % Caso DC
                     if strcmp(isla{Y},'DC')
-                        if ~isempty(obj.islas.estacionario.(isla{Y}).puertas)
+                        if ~isempty(obj.islas.estacionario.(isla{Y}).puertas)                         
                             % "x" recorre todas las capas de DC
                             for x = 1:size(obj.islas.estacionario.(isla{Y}),2)
-                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1) 
+                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1)
                                     switch obj.islas.estacionario.(isla{Y})(x).puertas(p,NAT_P)
                                         case {R,L,C}
                                             obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
@@ -133,43 +148,18 @@ classdef md_estacionario < hcs.modelo_datos
                             end
                         end
                     end
-                    
+
                     % Caso sinusoidal con igual o distinta frecuencia
                     if strcmp(isla{Y},'SIN')
-                        exc = {'igual_W' 'dif_W'};
+                        % Recordar que en esta versión se ignoran los campos
+                        % de igual y distinta frecuencia
+                        % exc = {'igual_W' 'dif_W'};
                         % e recorre igual_w y dif_W
-                        for E = 1:numel(exc)
-                            if ~isempty(obj.islas.estacionario.(isla{Y}).(exc{E}).puertas)
-                                % "x" recorre todas las capas de igual_W y dif_W
-                                for x = 1:size(obj.islas.estacionario.(isla{Y}).(exc{E}),2)
-                                    for p = 1:size(obj.islas.estacionario.(isla{Y})(x).(exc{E}).puertas,1)         
-                                        switch obj.islas.estacionario.(isla{Y})(x).(exc{E}).puertas(p,NAT_P)
-                                            case {R,L,C}
-                                                obj.clases.estacionario.(isla{Y})(x).(exc{E}).puertas{p,1} = @md_puerta_rlc;
-                                            case {VI,II}
-                                                obj.clases.estacionario.(isla{Y})(x).(exc{E}).puertas{p,1} = @md_puerta_fte_indep;
-                                            case {VD,ID}
-                                                obj.clases.estacionario.(isla{Y})(x).(exc{E}).puertas{p,1} = @md_puerta_fte_dep;
-                                            case LM
-                                                obj.clases.estacionario.(isla{Y})(x).(exc{E}).puertas{p,1} = @md_puerta_rlc;
-                                        end
-                                    end
-                                    if ~isempty(obj.islas.estacionario.(isla{Y})(x).(exc{E}).marcas_pol)
-                                        for i = 1:size(obj.islas.estacionario.(isla{Y})(x).(exc{E}).marcas_pol,1)
-                                            obj.clases.estacionario.(isla{Y})(x).(exc{E}).marcas_pol{i,1} = @hcs.marcas_polaridad;
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                    
-                    % Caso EXP
-                    if strcmp(isla{Y},'EXP')
+
                         if ~isempty(obj.islas.estacionario.(isla{Y}).puertas)
-                            % "x" recorre todas las capas de EXP
+                            % "x" recorre todas las capas de igual_W y dif_W
                             for x = 1:size(obj.islas.estacionario.(isla{Y}),2)
-                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1) 
+                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1)
                                     switch obj.islas.estacionario.(isla{Y})(x).puertas(p,NAT_P)
                                         case {R,L,C}
                                             obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
@@ -180,22 +170,51 @@ classdef md_estacionario < hcs.modelo_datos
                                         case LM
                                             obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
                                     end
-                                    if ~isempty(obj.islas.estacionario.(isla{Y})(x).marcas_pol)
-                                        for i = 1:size(obj.islas.estacionario.(isla{Y})(x).marcas_pol,1)
-                                            obj.clases.estacionario.(isla{Y})(x).marcas_pol{i,1} = @hcs.marcas_polaridad;
-                                        end
+                                end
+                                if ~isempty(obj.islas.estacionario.(isla{Y})(x).marcas_pol)
+                                    for i = 1:size(obj.islas.estacionario.(isla{Y})(x).marcas_pol,1)
+                                        obj.clases.estacionario.(isla{Y})(x).marcas_pol{i,1} = @hcs.marcas_polaridad;
                                     end
                                 end
                             end
                         end
                     end
-                    
-                    % Caso No_Fuente
-                    if strcmp(isla{Y},'No_Fuente')
-                        if ~isempty(obj.islas.estacionario.(isla{Y}).puertas)
+
+                    % Caso EXP
+                    % if strcmp(isla{Y},'EXP')
+                    %     if isempty(obj.islas.estacionario.(isla{Y}).puertas)
+                    %         obj.islas.estacionario.(isla{Y}).puertas = [];
+                    %     else
+                    %         % "x" recorre todas las capas de EXP
+                    %         for x = 1:size(obj.islas.estacionario.(isla{Y}),2)
+                    %             for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1)
+                    %                 switch obj.islas.estacionario.(isla{Y})(x).puertas(p,NAT_P)
+                    %                     case {R,L,C}
+                    %                         obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
+                    %                     case {VI,II}
+                    %                         obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_fte_indep;
+                    %                     case {VD,ID}
+                    %                         obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_fte_dep;
+                    %                     case LM
+                    %                         obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
+                    %                 end
+                    %                 if ~isempty(obj.islas.estacionario.(isla{Y})(x).marcas_pol)
+                    %                     for i = 1:size(obj.islas.estacionario.(isla{Y})(x).marcas_pol,1)
+                    %                         obj.clases.estacionario.(isla{Y})(x).marcas_pol{i,1} = @hcs.marcas_polaridad;
+                    %                     end
+                    %                 end
+                    %             end
+                    %         end
+                    %     end
+                    % end
+
+                    % Caso NoFuente
+                    if strcmp(isla{Y},'NoFuente')
+                        if ~isempty(obj.islas.estacionario.(isla{Y}))
+                            %obj.islas.estacionario.(isla{Y}).puertas = [];
                             % "x" recorre todas las capas de No_fuente
                             for x = 1:size(obj.islas.estacionario.(isla{Y}),2)
-                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1) 
+                                for p = 1:size(obj.islas.estacionario.(isla{Y})(x).puertas,1)
                                     switch obj.islas.estacionario.(isla{Y})(x).puertas(p,NAT_P)
                                         case {R,L,C}
                                             obj.clases.estacionario.(isla{Y})(x).puertas{p,1} = @md_puerta_rlc;
